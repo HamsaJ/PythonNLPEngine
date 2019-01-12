796ed548e95b844824c949bd220642b8eaa2d265
Jama <Hamsa Jama>
2019-01-12 21:11:41 +0000

    initial commit

---
+++ src/core/__init__.py
@@ -0,0 +1,9 @@
+import spacy
+from spacy.lang.en import English
+from .pipelines import RESTCountriesComponent
+
+ner_model = spacy.load('en_core_web_md')
+
+nlp = English()
+rest_countries = RESTCountriesComponent(nlp, label='COUNTRY')
+nlp.add_pipe(rest_countries)
---
+++ src/core/models/train.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf8
+"""Example of training spaCy's named entity recognizer, starting off with an
+existing model or a blank model.
+
+For more details, see the documentation:
+* Training: https://spacy.io/usage/training
+* NER: https://spacy.io/usage/linguistic-features#named-entities
+
+Compatible with: spaCy v2.0.0+
+"""
+from __future__ import unicode_literals, print_function
+
+import plac
+import random
+from pathlib import Path
+import spacy
+from spacy.util import minibatch, compounding
+
+
+# training data
+TRAIN_DATA = [
+    ("Who is Shaka Khan?", {"entities": [(7, 17, "PERSON")]}),
+    ("I like London and Berlin.", {"entities": [(7, 13, "LOC"), (18, 24, "LOC")]}),
+]
+
+
+@plac.annotations(
+    model=("Model name. Defaults to blank 'en' model.", "option", "m", str),
+    model_type=("Model name. Defaults to blank 'en' model.", "option", "t", str),
+    output_dir=("Optional output directory", "option", "o", Path)
+)
+def main(model=None, model_type=None, output_dir=None):
+    """Load the model, set up the pipeline and train the entity recognizer."""
+    if model is not None:
+        nlp = spacy.load(model)  # load existing spaCy model
+        print("Loaded model '%s'" % model)
+    else:
+        nlp = spacy.blank("en")  # create blank Language class
+        print("Created blank 'en' model")
+
+    # ner = nlp.create_pipe(model_type)
+    # nlp.add_pipe(ner, last=True)
+
+    # save model to output directory
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        if not output_dir.exists():
+            output_dir.mkdir()
+        nlp.to_disk(output_dir)
+        print("Saved model to", output_dir)
+
+
+if __name__ == "__main__":
+    plac.call(main)
